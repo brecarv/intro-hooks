198dba9008ad4d476bc2354d8e51ac7386a9f6f5
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-04-02 20:23:14 -0300

    component without hooks

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import Counter from "../counter/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <Counter></Counter>
+    </>
+  );
+}
+
+export default App;
